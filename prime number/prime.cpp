50a1ba58bd9777919e85a5d5b56dde00b20fc448
niks <nikarora1111@gmail.com>
2018-10-13 01:44:58 +0530

    checking prime number using sieve method

---
+++ prime number/prime.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long long int x,n;
+    cin>>x>>n;
+    bool arr[n+1];
+    long long int i,j;
+    arr[0]=false;
+    arr[1]=false;
+    for(i=2;i<=n;i++)
+    arr[i]=true;
+    for(j=2;j<=n;j++)
+    {
+        if(arr[j]==true)
+        {
+        for(i=j+j;i<=n;i=i+j)
+        {
+            arr[i]=false;
+        }
+        }
+    }
+    for(i=x;i<n+1;i++)
+    {if(arr[i]==true)
+    cout<<i<<endl;}
+	cout<<endl;
+	return 0;
+}
